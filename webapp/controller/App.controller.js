62658449bed2467df2eabdc547c11b41004e21db
Arf Mahamud <fmarif2009@gmail.com>
2023-11-09 21:54:01 +0700

    commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function(Controller) {
+    'use strict';
+
+    return Controller.extend("ui5.tutorial.controller.App",{
+        onShowHello(){
+            alert("Hello World");
+        }
+
+    });
+    
+    
+
+
+});
---
+++ webapp/index.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+    
+], (XMLView) => {
+    "use strict";
+    XMLView.create({
+        viewName: "ui5.tutorial.view.App"
+    }).then((oView) => oView.placeAt("content"));
+
+    
+});
